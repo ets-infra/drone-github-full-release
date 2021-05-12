39cb8fb6cca181b63336f845e3d380cf20e7f26e
Colin-b <Colin-b@users.noreply.github.com>
2021-05-12 17:59:30 +0200

    First release

---
+++ 0/switch_to_full_release.py
@@ -0,0 +1,62 @@
+import os
+
+import keepachangelog
+import requests
+
+
+class GitHub:
+    def __init__(self):
+        token = os.getenv('PLUGIN_GITHUB_TOKEN', os.getenv("DRONE_GIT_PASSWORD"))
+        if not token:
+            raise Exception("github_token parameter must be provided as DRONE_GIT_PASSWORD environment variable is not set.")
+
+        github_link = os.getenv("DRONE_REPO_LINK")[:-len(os.getenv("DRONE_REPO"))]
+        self.base_url = f"{github_link}api/v3/repos/{os.getenv('DRONE_REPO')}"
+        self.client = requests.Session()
+        self.client.headers = {'Authorization': f"token {token}", "Accept": "application/vnd.github.v3+json"}
+
+    def get(self, uri: str) -> requests.Response:
+        response = self.client.get(
+            url=f"{self.base_url}{uri}",
+        )
+        response.raise_for_status()
+        return response
+
+    def patch(self, uri: str, content: dict) -> requests.Response:
+        response = self.client.patch(
+            url=f"{self.base_url}{uri}",
+            json=content
+        )
+        if not response:
+            raise Exception(f"Unable to PATCH to {response.url}: {response.text} (HTTP {response.status_code})")
+        response.raise_for_status()
+        return response
+
+    def release(self, tag: str, new_title: str):
+        release_id = self.get(f"/releases/tags/{tag}").json()["id"]
+        self.patch(
+            f"/releases/{release_id}",
+            {
+                "name": new_title,
+                "prerelease": False
+            }
+        )
+
+
+def create_github_release():
+    release = get_latest_version(os.getenv('PLUGIN_CHANGELOG_PATH', "CHANGELOG.md"))
+
+    GitHub().release(
+        tag=release["version"],
+        new_title=f"{release['version']} ({release['release_date']})",
+    )
+
+
+def get_latest_version(changelog_path: str) -> dict:
+    releases = keepachangelog.to_raw_dict(changelog_path)
+    new_version = sorted(releases.keys())[-1]
+    return releases[new_version]
+
+
+if __name__ == "__main__":
+    create_github_release()
---
+++ README.md
@@ -0,0 +1,42 @@
+# Supported tags and respective Dockerfile links
+
+- [`0.0.1`, `latest`](https://github.com/ets-infra/drone-github-full-release/blob/master/0/Dockerfile)
+
+# Quick reference (cont.)
+
+- **Where to file issues**: [https://github.com/ets-infra/drone-github-full-release/issues](https://github.com/ets-infra/drone-github-full-release/issues)
+
+# What is the purpose of this image?
+
+[Drone](https://www.drone.io) plugin to switch a GitHub pre-release to a full-release.
+
+<p align="center">
+    <a href="https://www.drone.io"><img alt="drone logo" src="https://raw.githubusercontent.com/drone/brand/master/logos/png/dark/drone-logo-png-dark-128.png"></a>
+</p>
+
+The following steps are executed by this plugin:
+
+1. Gather the latest version number based on changelog.
+2. Update the GitHub release for this version to a full release.
+
+| Parameter | Description |
+|:---|---|
+| changelog_path | Path to the changelog. Default to `CHANGELOG.md` in current folder. |
+| github_token | Token (repo permission) used to update the release. Default to [the drone GIT password](https://docs.drone.io/server/reference/drone-git-password/) (if available). Related user needs to have admin role in repository. |
+
+# How to use this image
+
+## Add a step to your drone pipeline
+
+```yaml
+kind: pipeline
+type: docker
+name: default
+
+steps:
+- name: tag
+  image: etsinfra/drone-github-full-release:latest
+  settings:
+    changelog_path: custom_folder/CHANGELOG.md
+    github_token: cc1cc11111111ccc1c11c1cc1ccc1c1cc1111c1c
+```
